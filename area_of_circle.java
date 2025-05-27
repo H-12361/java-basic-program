a418674ceb063c42e59e5fba7edb513d7946d8e5
H-12361 <harshittiwariharshittiwari8@gmail.com>
2025-05-27 08:41:13 +0530

    update day 2

---
+++ area_of_circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class area_of_circle {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    float radius = sc.nextFloat();
+    float area = 3.14f * radius * radius; // java treat as a float if  use 3.14;
+   
+   System.out.println(area);
+    
+
+   }
+}
--- inputinjava.java
+++ inputinjava.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class  inputinjava {
 
 public static void main(String args[]){
-     Scanner sc=new Scanner(System.in);
+     Scanner sc =new Scanner(System.in);
    // String input=sc.next(); //harshit it print only single accourance of space
    //String input =sc.nextLine(); //you can print Harshit Tiwari
    //System.out.println(input);
